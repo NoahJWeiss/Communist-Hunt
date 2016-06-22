947bae728a4253244991d4b4fc5ae9c734437ed7
cooperchia <cooperchia3@gmail.com>
2016-06-21 21:00:23 -0700

    Add files via upload

--- RulesListener.java
+++ RulesListener.java
@@ -1,6 +1,15 @@
 import javax.swing.*;
 import java.awt.event.*;
 
+/**
+ * RulesListener.java
+ * Summary: RulesListener is called when the user clicks the "Rules" button
+ * on the game screen, the rules of the game are displayed in a pane.
+ * @version 06/21/16
+ * @author Cooper Chia, Griffin Craft, Noah Weiss
+ */
+
+
 public class RulesListener implements ActionListener {
    //opens up a pane that shows the rules when the user clicks the "Rules" button
    public void actionPerformed(ActionEvent event) {
