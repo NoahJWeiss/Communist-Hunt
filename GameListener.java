e47709917af76f7fb2652cc7944ccc0797c860cf
cooperchia <cooperchia3@gmail.com>
2016-06-21 21:00:03 -0700

    Add files via upload

--- GameListener.java
+++ GameListener.java
@@ -1,6 +1,15 @@
 import java.awt.event.*;
 import java.io.*;
 
+/**
+ * GameListener.java
+ * Summary: GameListener is called when the user clicks the 
+ * "New Game" button on the game screen, starts a new game
+ * @version 06/21/16
+ * @author Cooper Chia, Griffin Craft, Noah Weiss
+ */
+
+
 public class GameListener implements ActionListener{
    //starts a new game when the user clicks the "New Game" button
    public void actionPerformed(ActionEvent event){
