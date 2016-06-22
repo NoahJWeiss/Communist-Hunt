a82dc580ed8ed36ff7f238fd5a872d087530b3aa
cooperchia <cooperchia3@gmail.com>
2016-06-21 20:59:54 -0700

    Add files via upload

--- ExitListener.java
+++ ExitListener.java
@@ -1,5 +1,13 @@
 import java.awt.event.*;
 
+/**
+ * ExitListener.java
+ * Summary: ExitListener is called when the user clicks
+ * the "Exit" button on the game screen, exits the game
+ * @version 06/21/16
+ * @author Cooper Chia, Griffin Craft, Noah Weiss
+ */
+
 public class ExitListener implements ActionListener{
    //Exits the game screen when the user clicks the "Exit" button in the frame
    public void actionPerformed(ActionEvent e){
